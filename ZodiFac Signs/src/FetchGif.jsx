73bbad94d0a19360a9421f0604c4f59e2ff7f722
Oli Jones <78092825+duckRabbitPy@users.noreply.github.com>
2022-02-09 17:44:34 +0000

    Add Giphy fetch
    Co-Authored-By: Juliette Orpen <53294140+julietteorpen@users.noreply.github.com>
    Co-Authored-By: MillyP (she/her) <palmermilly9@gmail.com>

--- ZodiFac Signs/src/FetchGif.jsx
+++ ZodiFac Signs/src/FetchGif.jsx
@@ -3,9 +3,21 @@ import React from "react";
 function FetchGif({ starsign }) {
   const [gif, setGif] = React.useState("");
 
-  React.useEffect(() => {}, [starsign]);
+  React.useEffect(() => {
+    if (starsign.length > 0) {
+      fetch(
+        `https://api.giphy.com/v1/gifs/search?q=${starsign}&api_key=${
+          import.meta.env.VITE_GIPHY_API_KEY
+        }`
+      )
+        .then((response) => response.json())
+        .then((response) => {
+          setGif(response.data[10].images.downsized.url);
+        });
+    }
+  }, [starsign]);
 
-  return <div>hello</div>;
+  return <img src={gif}></img>;
 }
 
 export default FetchGif;
