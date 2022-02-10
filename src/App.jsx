99783ccfd6fe00e73f4c1e44b36f238d4aea323e
Oli Jones <78092825+duckRabbitPy@users.noreply.github.com>
2022-02-10 17:14:20 +0000

    hide date picker after form submit
    Co-Authored-By: Juliette Orpen <53294140+julietteorpen@users.noreply.github.com>
    Co-Authored-By: MillyP (she/her) <palmermilly9@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,7 @@ function App() {
           </div>
         )}
       </div>
-      <PickSign setStarField={setStarField}></PickSign>
+      {formVisible && <PickSign setStarField={setStarField}></PickSign>}
     </div>
   );
 }
